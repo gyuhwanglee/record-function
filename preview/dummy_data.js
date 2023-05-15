e2e59f12104b336d8843f68c78e876be01deebc0
gyuhwanglee <79841624+gyuhwanglee@users.noreply.github.com>
2023-05-15 21:37:10 +0900

    add dummy_data.js

---
+++ preview/dummy_data.js
@@ -0,0 +1,98 @@
+export const coupang_data = [
+  {
+    "title": "coupang_representative_img",
+    "contents": "https://image6.coupangcdn.com/image/resized_image/thumbnails/remote/1180x2360q60crop1180x630/image/travelSeller/common/A00218293/c7ee2544-8697-4342-99a4-f8f15f3cd619.jpg",
+  },
+  {
+    "title": "coupang_title",
+    "contents": "제주도 버스투어 원데이 일일투어 패키지 / 서쪽 동쪽 당일치기 뚜벅이 여행",
+  },
+  {
+    "title": "coupang_side_date",
+    "contents": "",
+  },
+  {
+    "title": "coupang_adress",
+    "contents": "제주특별자치도 제주시 다랑곶길 26-1",
+  },
+  {
+    "title": "coupang_price",
+    "contents": "39,900",
+  },
+  {
+    "title": "coupang_price_standard",
+    "contents": "1. 서귀포 출발 동쪽 코스 (홀수일) 기준",
+  },
+  {
+    "title": "coupang_promotion",
+    "contents": "삼다수 서비스",
+  },
+  {
+    "title": "coupang_introduce",
+    "contents": "당일치기로 핫플레이스만 즐기는 '하루버스'의 원데이 일일투어입니다. 추가요금 없으며 여행 불만족시 전액 환불해드립니다. '하루버스'는 요일에 관계없이 서쪽코스, 동쪽코스 모두 매일 운행합니다. 중식비는 미포함이며 자유롭게 식사를 하시면 됩니다. '하루버스'는 1인부터 출발합니다.",
+  },
+  {
+    "title": "coupang_side_notice",
+    "contents": "이용날짜와 코스선택 후 인원수에 맞춰 구매하시면 됩니다. 결제와 동시에 예약 확정됩니다. 하루전 예약시에는 꼭 연락 부탁드립니다. 문의: 010-3151-1239",
+  },
+  {
+    "title": "coupang_date",
+    "contents": "2월 4일 (금)",
+  },
+  {
+    "title": "coupang_product_title1",
+    "contents": "1. 서귀포 출발 동쪽 코스 (홀수일)",
+  },
+  {
+    "title": "coupang_product_title2",
+    "contents": "2. 서귀포 출발 서쪽 코스 (짝수일)",
+  },
+  {
+    "title": "coupang_product_date",
+    "contents": "",
+  },
+  {
+    "title": "coupang_product_info",
+    "contents": "",
+  },
+  {
+    "title": "coupang_product_addinfo",
+    "contents": "",
+  },
+  {
+    "title": "coupang_product_price1",
+    "contents": "39,900",
+  },
+  {
+    "title": "coupang_product_price2",
+    "contents": "39,900",
+  },
+  {
+    "title": "coupang_main_img",
+    "contents": "https://image15.coupangcdn.com/image/travelSeller/common/A00218293/254c5ee5-21c1-49c3-8541-b60565b21292.jpg",
+  },
+  {
+    "title": "coupang_includ",
+    "contents": "- 픽업 및 드랍 비용, 일정상 관광지 입장료, 프로그램 진행 차량, 프로그램 진행 드라이빙 가이드",
+  },
+  {
+    "title": "coupang_notincluded",
+    "contents": " -중식 비용, 개인 여행 경비, 성산일출봉 유료탐방구간 입장료(중식 시간으로 정상까지 오르기에는 시간이 부족합니다) -우도, 드르쿰다in성산, 마라도, 돌고래요트 관련 비용",
+  },
+  {
+    "title": "coupang_notice",
+    "contents": "  - 이용 당일 픽업 장소에서 출발 10분 전까지 '하루버스'에 탑승하시기 바랍니다. - 기상악화, 천재지변 등의 현지 사정으로 투어가 취소될 수도 있습니다.(투어 취소 시 개별적으로 연락드리며 전액 환불됩니다) - 출발/하차 지정 장소, 픽업 지정 장소 이외의 개별 픽업 서비스는 불가합니다. - 버스 탑승 시 원하시는 자리에 편하게 앉으셔서 투어를 하시면 됩니다. - 캐리어 등의 짐은 트렁크에 보관이 가능 합니다. - 48개월 미만의 소아는 무료이며(단 예약 시 미리 말씀해주셔야합니다) 48개월 이상은 이용요금이 동일합니다. ",
+  },
+  {
+    "title": "coupang_company_info",
+    "contents": " • 구매와 동시에 예약이 확정됩니다. - 쿠팡에서 원하는 날짜와 옵션(코스) 선택 후 쿠폰 구매 (예: 3명 이용 시 쿠폰 3개 구매) - 쿠폰 구매 후 남겨주신 성함과 연락처로 콜센터에서 투어 하루전까지 연락을 드리거나 안내 문자를 발송해 드립니다. - 콜센터: 010-3151-1239 - 이용 당일 픽업 장소에서 성함과 연락처 확인 후 즐거운 여행 출발~ 문의전화 : 010-3151-1239 고객센터 문의시간 : 오전 10시~오후 5시 문의시간 외에는 메시지 남겨주시면 연락드립니다.",
+  },
+  {
+    "title": "coupang_cancel_info",
+    "contents": "• 미사용 티켓 환불제 적용상품으로 사용기간 종료 3일 후(영업일 기준), 구매 금액의 100%를 쿠팡캐시로 적립해드립니다. 단, 회원 탈퇴 시에는 적립금 환불을 받을 수 없습니다. (사용기간 종료 7일 이내 구매 시 100% 원결제 수단으로 환불해드립니다.) [취소/환불 규정] 딜 진행중에는 '마이쿠팡'내에서 취소/환불이 가능하며, 딜 종료 후 유효기간 이내에는 유선상으로 취소/환불이 가능합니다. (쿠팡고객센터: 1577-7011) [미사용 티켓 환불제 적용상품] 유효기간 내 사용하지 못한 티켓은 유효기간 종료 3일후, 구매 금액의 100%를 쿠팡 적립금으로 돌려드립니다. ​​단, ​사용​기간 종료일 기준 7일 이내에 구매하신 고객의 경우 100% 환불하여 드립니다. (쿠팡고객센터 : 1577-7011)",
+  },
+  {
+    "title": "coupang_cancel_notice",
+    "contents": "취소 수수료는 총 결제금액 기준으로 책정됩니다. 단 즉시할인 상품의 경우 할인 적용 전 금액으로 책정됩니다. 패키지로 구성된 상품은 부분환불 불가 합니다. (구성품 중 일부 사용시 부분환불 또는 전체환불 불가) 취소 마감일 : 영업일과 주말(토, 일요일) 및 공휴일 취소 가능. 취소 마감 시간 : 23시 59분 기준.",
+  },
+];
